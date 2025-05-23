67e40dd36d172d6239083ce660a4f6bce7213ed7
Astrian Zheng <happy.wheel6743@astrian.moe>
2025-05-24 08:59:17 +1000

    fix: linting issue

--- public/background.js
+++ public/background.js
@@ -1,13 +1,23 @@
-console.log("background.js loaded 55555")
+console.log('background.js loaded 55555')
 
 chrome.webRequest.onBeforeRequest.addListener(
-	function(details) {
-		console.log("onBeforeRequest MAIN_FRAME:", details.url, details.type, details.frameId, details.tabId)
-		if (details.url === "https://monster-siren.hypergryph.com/manifest.json" && details.type === "other" && details.frameId === 0) {
-			console.log("onBeforeRequest - REDIRECTING MAIN_FRAME")
-			chrome.tabs.create({ url: chrome.runtime.getURL("index.html") })
+	(details) => {
+		console.log(
+			'onBeforeRequest MAIN_FRAME:',
+			details.url,
+			details.type,
+			details.frameId,
+			details.tabId,
+		)
+		if (
+			details.url === 'https://monster-siren.hypergryph.com/manifest.json' &&
+			details.type === 'other' &&
+			details.frameId === 0
+		) {
+			console.log('onBeforeRequest - REDIRECTING MAIN_FRAME')
+			chrome.tabs.create({ url: chrome.runtime.getURL('index.html') })
 			chrome.tabs.remove(details.tabId)
 		}
 	},
-	{ urls: ["https://monster-siren.hypergryph.com/manifest.json"] }
+	{ urls: ['https://monster-siren.hypergryph.com/manifest.json'] },
 )
