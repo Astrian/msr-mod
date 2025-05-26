1a700659ae984750d7c9bd0f1638b30091e9a1c5
Astrian Zheng <happy.wheel6743@astrian.moe>
2025-05-26 10:43:57 +1000

    refactor(icons): 移除图标组件中的冗余div包装层
    优化图标组件结构，直接将尺寸类应用于svg元素

--- src/assets/icons/pause.vue
+++ src/assets/icons/pause.vue
@@ -5,9 +5,7 @@ defineProps<{
 </script>
 
 <template>
-	<div :class="`w-${size} h-${size}`">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-			<path d="M6 5H8V19H6V5ZM16 5H18V19H16V5Z"></path>
-		</svg>
-	</div>
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" :class="`w-${size} h-${size}`">
+		<path d="M6 5H8V19H6V5ZM16 5H18V19H16V5Z"></path>
+	</svg>
 </template>
