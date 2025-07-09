a139d1278ad137c208774e03bc965546dd15faf7
Astrian Zheng <happy.wheel6743@astrian.moe>
2025-07-09 16:51:11 +1000

    add: fundamental of mini player ui

---
+++ src/components/MiniPlayer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div
+		class="h-9 w-52 bg-neutral-800/80 border border-[#ffffff39] rounded-full backdrop-blur-3xl flex items-center justify-between select-none">
+		<div class="flex items-center gap-2">
+			<div class="rounded-full w-9 h-9 bg-gray-600" />
+			<div class="text-white">Song title</div>
+		</div>
+	</div>
+</template>
